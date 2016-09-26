092cb2c56f9db0dac13c51b1fcb53972bd3e60b4
gaojicai1 <gaojicai1@yhd.com>
2016-09-26 09:17:25 +0800

    update

--- HiService/foundation/src/main/java/cn/com/hiserivice/hiservice/view/CustomNavView.java
+++ HiService/foundation/src/main/java/cn/com/hiserivice/hiservice/view/CustomNavView.java
@@ -61,10 +61,88 @@ public class CustomNavView extends LinearLayout {
         mRightItem.addOnLayoutChangeListener(layoutChangeListener);
     }
 
+    public LinearLayout getmLeftItem() {
+        return mLeftItem;
+    }
+
+    public TextView getmLeftItemTitleView() {
+        return mLeftItemTitleView;
+    }
+
+    public ImageView getmLeftItemImgView() {
+        return mLeftItemImgView;
+    }
+
+    public FrameLayout getmRightItem() {
+        return mRightItem;
+    }
+
+    public ImageView getmRightItemImgView() {
+        return mRightItemImgView;
+    }
+
+    public TextView getmRightViewTitleView() {
+        return mRightViewTitleView;
+    }
+
+    public FrameLayout getmMiddleConainer() {
+        return mMiddleConainer;
+    }
+
+    public TextView getmMiddleTitleView() {
+        return mMiddleTitleView;
+    }
+
     OnLayoutChangeListener layoutChangeListener = new OnLayoutChangeListener() {
         @Override
         public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
 
         }
     };
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void setMidAlways(boolean midAlways) {
+        this.midAlways = midAlways;
+    }
+
+    public CustomNavView reset() {
+        this.setVisibility(VISIBLE);
+
+        mLeftItemTitleView.setVisibility(GONE);
+        mLeftItemTitleView.setText(null);
+        mLeftItemImgView.setVisibility(GONE);
+        mLeftItemImgView.setImageDrawable(null);
+
+        mLeftItem.setOnClickListener(null);
+        mLeftItem.removeAllViews();
+        mLeftItem.setMinimumWidth(0);
+        mLeftItem.addView(mLeftItemTitleView);
+        mLeftItem.addView(mLeftItemImgView);
+
+        mRightViewTitleView.setVisibility(GONE);
+        mRightViewTitleView.setText(null);
+        mRightItemImgView.setVisibility(GONE);
+        mRightItemImgView.setImageDrawable(null);
+
+        mRightItem.setOnClickListener(null);
+        mRightItem.removeAllViews();
+        mRightItem.setMinimumWidth(0);
+        mRightItem.addView(mRightItemImgView);
+        mRightItem.addView(mRightViewTitleView);
+
+        mMiddleTitleView.setVisibility(GONE);
+        mMiddleTitleView.setText(null);
+        mMiddleConainer.removeAllViews();
+        midAlways = true;
+        mMiddleConainer.addView(mMiddleTitleView);
+        return this;
+    }
+
+    public interface ICustomNavBar {
+        void customNacBar(CustomNavView navView);
+    }
 }
